6b912b031bed6a68755e7e31d45f923bf25ba949
sayed123 <sayedmohameddev@gmai.com>
2025-01-01 22:24:03 +0200

    q-02

--- assi-oop-1/Program.cs
+++ assi-oop-1/Program.cs
@@ -14,6 +14,16 @@ namespace assi_oop_1
     //    Sunday
     //}
     #endregion
+    #region
+    //enum Season
+    //{
+    //    Spring,
+    //    Summer,
+    //    Autumn,
+    //    Winter
+    //}
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
@@ -25,6 +35,36 @@ namespace assi_oop_1
             //{
             //    Console.WriteLine(day);
             //}
+            #endregion
+
+            #region q-02
+
+            //Console.WriteLine("Enter a season (Spring, Summer, Autumn, Winter):");
+            //string userInput = Console.ReadLine();
+
+            //if (Enum.TryParse(userInput, true, out Season selectedSeason))
+            //{
+            //    switch (selectedSeason)
+            //    {
+            //        case Season.Spring:
+            //            Console.WriteLine("Spring: March to May");
+            //            break;
+            //        case Season.Summer:
+            //            Console.WriteLine("Summer: June to August");
+            //            break;
+            //        case Season.Autumn:
+            //            Console.WriteLine("Autumn: September to November");
+            //            break;
+            //        case Season.Winter:
+            //            Console.WriteLine("Winter: December to February");
+            //            break;
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid season name.");
+            //}
+
             #endregion
         }
     }
