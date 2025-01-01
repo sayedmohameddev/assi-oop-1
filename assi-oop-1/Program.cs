24d1d99ff95c2d4a004fcf86e00f128bb316bbe2
sayed123 <sayedmohameddev@gmai.com>
2025-01-01 22:21:08 +0200

    q-01

--- assi-oop-1/Program.cs
+++ assi-oop-1/Program.cs
@@ -2,11 +2,30 @@
 
 namespace assi_oop_1
 {
+    #region 
+    //enum WeekDays
+    //{
+    //    Monday,
+    //    Tuesday,
+    //    Wednesday,
+    //    Thursday,
+    //    Friday,
+    //    Saturday,
+    //    Sunday
+    //}
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region q-01
+            //Console.WriteLine("Days of the Week:");
+
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+            #endregion
         }
     }
 }
