849f307d59ae0859c61e1c9fe821c4a8d6597d41
DELL <DELL@DESKTOP-SK90L8L>
2025-01-01 22:15:46 +0200

    Add project files.

---
+++ assi-oop-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assi_oop_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
